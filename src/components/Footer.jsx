0816a7a0705846a1a9552c955ed8591a9a6281cb
Valeria Bielousova <lerusia136@gmail.com>
2025-12-05 12:50:20 +0200

    lab 12

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
     return (
         <footer className="footer">
             <div className="footer__inner">
-                <p className="footer__brand">© {currentYear} Lab 11 by Bielousova Valeria</p>
+                <p className="footer__brand">© {currentYear} Lab 12 by Bielousova Valeria</p>
                 <p>Побудовано на Vite + React</p>
             </div>
         </footer>
